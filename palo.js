9162bc39f1c5f2b6e0b9a51fcd897ccc12b87ec4
BradSlavens <bradslavens@gmail.com>
2022-04-11 07:06:39 -0700

    works, ready to test

--- palo.js
+++ palo.js
@@ -3,35 +3,70 @@
  * @return {string}
  */
  var longestPalindrome = function(s) {
-    let p = []
-    for(let i = 0; i<s.length; i++){
-        let substrings = s[i]
-        // console.log(`substring i ${s[i]}`)
-        p.push(s[i])
-        for(let j = i + 1; j<s.length;  j++){
-            substrings+= s[j]
-            let revSub = reverseSubStr(substrings)
-            // console.log(`revSub ${revSub} substrings ${substrings}`)
-            if(revSub == substrings) p.push(substrings)            
-        }
-        
-    }
-    // get the longest
-    let longest = ""
-    for(let i = 0; i < p.length; i++){
-        if(p[i].length > longest.length ) longest = p[i]
-        // console.log(`longest ${p}`)
-    }
-    
+    let arr = [...s]
+    let subArrs = getSubArrs(arr)
+    let revSubArrs = getRevArrs(subArrs)
+    let pals = getPals(subArrs, revSubArrs)
+    let longestPalArr = getLongestPal(pals)
+    let longest = makeString(longestPalArr)
     return longest
 };
 
-function reverseSubStr(subStr){
-    let revSub = ""
-    for(let i = subStr.length - 1; i >= 0; i--){
-        revSub+=subStr[i]
+function makeString (longestPalArr){
+    let str = ""
+    for( let i = 0; i<longestPalArr.length; i++){
+        str+=longestPalArr[i]
     }
-    return revSub
+    return str
 }
 
-console.log(`answer ${longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip")}`)
+function getLongestPal(pals){
+    longestPal = []
+    for(let i = 0; i<pals.length; i++){
+        if(pals[i].length > longestPal.length) longestPal = pals[i]
+    }
+    return longestPal
+}
+function getPals(subArrs, revSubArrs){
+    let pals = []
+        for(let i = 0; i<subArrs.length; i++){
+            let pal = []
+            for(let j = 0; j<subArrs[i].length; j++){
+                if(subArrs[i][j] !== revSubArrs[i][j]) break
+                pal.push(subArrs[i][j])
+            }
+            pals.push(pal)
+        }
+    return pals
+}
+
+function getRevArrs(subArrs){
+    let revSubs = []
+    for(let i = 0; i<subArrs.length; i++){
+        let revSub = []
+        for(let j = subArrs[i].length-1; j>=0; j--){
+            revSub.push(subArrs[i][j])
+        }
+        revSubs.push(revSub)
+    }
+    return revSubs
+}
+
+function getSubArrs(arr){
+    let subs = []
+    for(let i = 0; i<arr.length; i++){
+        subs.push([arr[i]])
+        let sub = [arr[i]]
+        for(let j = i + 1; j<arr.length; j++ ){
+            sub = [...sub, arr[j]]
+            subs.push(sub)
+        }
+    }
+
+    return subs
+}
+
+
+
+// console.log(`answer ${longestPalindrome("rgczcpratwyqxaszbuwwcadruayhasynuxnakpmsyhxzlnxmdtsqqlmwnbxvmgvllafrpmlfuqpbhjddmhmbcgmlyeypkfpreddyencsdmgxysctpubvgeedhurvizgqxclhpfrvxggrowaynrtuwvvvwnqlowdihtrdzjffrgoeqivnprdnpvfjuhycpfydjcpfcnkpyujljiesmuxhtizzvwhvpqylvcirwqsmpptyhcqybstsfgjadicwzycswwmpluvzqdvnhkcofptqrzgjqtbvbdxylrylinspncrkxclykccbwridpqckstxdjawvziucrswpsfmisqiozworibeycuarcidbljslwbalcemgymnsxfziattdylrulwrybzztoxhevsdnvvljfzzrgcmagshucoalfiuapgzpqgjjgqsmcvtdsvehewrvtkeqwgmatqdpwlayjcxcavjmgpdyklrjcqvxjqbjucfubgmgpkfdxznkhcejscymuildfnuxwmuklntnyycdcscioimenaeohgpbcpogyifcsatfxeslstkjclauqmywacizyapxlgtcchlxkvygzeucwalhvhbwkvbceqajstxzzppcxoanhyfkgwaelsfdeeviqogjpresnoacegfeejyychabkhszcokdxpaqrprwfdahjqkfptwpeykgumyemgkccynxuvbdpjlrbgqtcqulxodurugofuwzudnhgxdrbbxtrvdnlodyhsifvyspejenpdckevzqrexplpcqtwtxlimfrsjumiygqeemhihcxyngsemcolrnlyhqlbqbcestadoxtrdvcgucntjnfavylip")}`)
+console.log(longestPalindrome("dad"))
