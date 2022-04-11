1b907a24cc31ee1f67f94f3615fc8c3cf84c250d
BradSlavens <bradslavens@gmail.com>
2022-04-11 08:49:06 -0700

    out of memory

--- palo.js
+++ palo.js
@@ -4,25 +4,27 @@
  */
  var longestPalindrome = function(s) {
     let arr = [...s]
-    let subArrs = getSubArrs(arr)
-    let revSubArrs = getRevArrs(subArrs)
-    let pals = getPals(subArrs, revSubArrs)
+    // console.log(arr)
+    let subStrs = getSubStrs(arr)
+    // console.log(subStrs)
+    let revSubStrs = getRevStrs(subStrs)
+    // console.log(revSubStrs)
+    let pals = getPals(subStrs, revSubStrs)
     // console.log(pals)
-    let longestPalArr = getLongestPal(pals)
-    let longest = makeString(longestPalArr)
+    let longest = getLongestPal(pals)
     return longest
 };
 
-function makeString (longestPalArr){
-    let str = ""
-    for( let i = 0; i<longestPalArr.length; i++){
-        str+=longestPalArr[i]
-    }
-    return str
-}
+// function makeString (longestPalArr){
+//     let str = ""
+//     for( let i = 0; i<longestPalArr.length; i++){
+//         str+=longestPalArr[i]
+//     }
+//     return str
+// }
 
 function getLongestPal(pals){
-    longestPal = []
+    longestPal = ""
     for(let i = 0; i<pals.length; i++){
         if(pals[i].length > longestPal.length) longestPal = pals[i]
     }
@@ -31,47 +33,36 @@ function getLongestPal(pals){
 function getPals(subArrs, revSubArrs){
     let pals = []
         for(let i = 0; i<subArrs.length; i++){
-            let pal = []
             // console.log(subArrs[i])
-            // console.log(`rev ${revSubArrs[i]} sub ${subArrs[i]} before loop`)
-            for(let j = 0; j<subArrs[i].length; j++){
-                // console.log(`after loop rev ${revSubArrs[i][j]} sub ${subArrs[i][j]}\n `)
-                if(subArrs[i][j] !== revSubArrs[i][j]) {
-                    // console.log(`not equal rev ${revSubArrs[i]} sub ${subArrs[i]}\n`)
-                    pal = []
-                    break;
-                }
-                pal.push(revSubArrs[i][j])
-                // console.log("pal " + pal + "\n\n")
-            }
-            if(pal.length > 0) pals.push(pal)
+            // console.log(revSubArrs[i])
+            if(subArrs[i] == revSubArrs[i]) pals.push(subArrs[i])
+            // console.log(pals)
         }
     return pals
 }
 
-function getRevArrs(subArrs){
+function getRevStrs(subArrs){
     let revSubs = []
     for(let i = 0; i<subArrs.length; i++){
-        let revSub = []
+        let revSub = ""
         for(let j = subArrs[i].length-1; j>=0; j--){
-            revSub.push(subArrs[i][j])
+            revSub+=subArrs[i][j]
         }
         revSubs.push(revSub)
     }
     return revSubs
 }
 
-function getSubArrs(arr){
+function getSubStrs(arr){
     let subs = []
     for(let i = 0; i<arr.length; i++){
-        subs.push([arr[i]])
-        let sub = [arr[i]]
+        subs.push(arr[i])
+        let sub = arr[i]
         for(let j = i + 1; j<arr.length; j++ ){
-            sub = [...sub, arr[j]]
+            sub += arr[j]
             subs.push(sub)
         }
     }
-
     return subs
 }
 
